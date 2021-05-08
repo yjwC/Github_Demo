1e1036062190f33d93261884823f4cde1634fb62
yjwC <1113480185@qq.com>
2021-05-08 10:46:57 +0800

    第一次提交

---
+++ src/main/java/com/fc/bean/User.java
@@ -0,0 +1,13 @@
+package com.fc.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String username;
+    private String password;
+}
